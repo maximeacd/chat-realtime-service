c33aa45c29ce00a04b01d342c56e13220511deb1
Maxinho94 <maxime.accad@gmail.com>
2025-09-11 18:58:03 +0200

    Initial commit – Spring Boot project setup

---
+++ src/main/java/com/chatapp/realtime_chat_service/RealtimeChatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chatapp.realtime_chat_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeChatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeChatServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatapp/realtime_chat_service/RealtimeChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatapp.realtime_chat_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
